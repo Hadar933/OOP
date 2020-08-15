31c331d3651b40b212c23690e247cc42d99148db
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-08-15 15:18:37 +0300

    working on spaceship.java
    added ctor

--- ex2/src/SpaceShip.java
+++ ex2/src/SpaceShip.java
@@ -1,20 +1,64 @@
 import java.awt.Image;
+
 import oop.ex2.*;
 
 /**
- * The API spaceships need to implement for the SpaceWars game. 
+ * The API spaceships need to implement for the SpaceWars game.
  * It is your decision whether SpaceShip.java will be an interface, an abstract class,
- *  a base class for the other spaceships or any other option you will choose.
- *  
+ * a base class for the other spaceships or any other option you will choose.
+ *
  * @author oop
  */
-public class SpaceShip{
+public class SpaceShip {
 
-   
     /**
-     * Does the actions of this ship for this round. 
+     * the image of the spaceship, that is being provided
+     */
+    public Image spaceShipImage;
+    /**
+     * the representation of position, direction and velocity of the ship
+     */
+    private SpaceShipPhysics physics;
+
+    /**
+     * The maximal energy level (starts at 210)
+     */
+    public int maxEnergy;
+
+    /**
+     * A current energy level, which is between 0 and the maximal energy level.
+     */
+    public int energy;
+    /**
+     * A Health level between 0 and 22.
+     */
+    private int health;
+
+    /**
+    /**
+     * the energy of initialization
+     */
+    private final int defaultEnergy = 210;
+
+    /**
+     * the maximum health level (when initialized)
+     */
+    private static final int defaultHealth = 22;
+
+    /*
+     * constructor for a space ship instance
+     */
+    SpaceShip() {
+        this.spaceShipImage = GameGUI.ENEMY_SPACESHIP_IMAGE;
+        this.physics = new SpaceShipPhysics();
+        this.energy = defaultEnergy;
+        this.health = defaultHealth;
+    }
+
+    /**
+     * Does the actions of this ship for this round.
      * This is called once per round by the SpaceWars game driver.
-     * 
+     *
      * @param game the game object to which this ship belongs.
      */
     public void doAction(SpaceWars game) {
@@ -22,23 +66,23 @@ public class SpaceShip{
     }
 
     /**
-     * This method is called every time a collision with this ship occurs 
+     * This method is called every time a collision with this ship occurs
      */
-    public void collidedWithAnotherShip(){
+    public void collidedWithAnotherShip() {
 
     }
 
-    /** 
-     * This method is called whenever a ship has died. It resets the ship's 
+    /**
+     * This method is called whenever a ship has died. It resets the ship's
      * attributes, and starts it at a new random position.
      */
-    public void reset(){
+    public void reset() {
 
     }
 
     /**
      * Checks if this ship is dead.
-     * 
+     *
      * @return true if the ship is dead. false otherwise.
      */
     public boolean isDead() {
@@ -47,7 +91,7 @@ public class SpaceShip{
 
     /**
      * Gets the physics object that controls this ship.
-     * 
+     *
      * @return the physics object that controls the ship.
      */
     public SpaceShipPhysics getPhysics() {
@@ -66,34 +110,34 @@ public class SpaceShip{
      * Gets the image of this ship. This method should return the image of the
      * ship with or without the shield. This will be displayed on the GUI at
      * the end of the round.
-     * 
+     *
      * @return the image of this ship.
      */
-    public Image getImage(){
+    public Image getImage() {
         return null;
     }
 
     /**
      * Attempts to fire a shot.
-     * 
+     *
      * @param game the game object.
      */
     public void fire(SpaceWars game) {
-       
+
     }
 
     /**
      * Attempts to turn on the shield.
      */
     public void shieldOn() {
-        
+
     }
 
     /**
      * Attempts to teleport.
      */
     public void teleport() {
-       
+
     }
-    
+
 }
--- ex2/src/SpaceShipFactory.java
+++ ex2/src/SpaceShipFactory.java
@@ -4,32 +4,34 @@ public class SpaceShipFactory {
     public static SpaceShip[] createSpaceShips(String[] args) {
         SpaceShip[] allShips = new SpaceShip[args.length];
         for (int i = 0; i < args.length; i++) {
-            if(args[i].equals("h")){
-                Human humanShip = new Human();
-                allShips[i] = humanShip;
-            }
-            else if(args[i].equals("r")){
-                Runner runnerShip = new Runner();
-                allShips[i] = runnerShip;
-            }
-            else if(args[i].equals("b")){
-                Basher basherShip = new Basher();
-                allShips[i] = basherShip;
-            }
-            else if(args[i].equals("a")){
-                Aggressive aggressiveShip = new Aggressive();
-                allShips[i] = aggressiveShip;
-            }
-            else if(args[i].equals("d")){
-                Drunkard drunkShip = new Drunkard();
-                allShips[i] = drunkShip;
-            }
-            else if(args[i].equals("s")){
-                Special specialShip = new Special();
-                allShips[i] = specialShip;
-            }
-            else{
-                allShips[i] = null;
+            switch (args[i]) {
+                case "h":
+                    Human humanShip = new Human();
+                    allShips[i] = humanShip;
+                    break;
+                case "r":
+                    Runner runnerShip = new Runner();
+                    allShips[i] = runnerShip;
+                    break;
+                case "b":
+                    Basher basherShip = new Basher();
+                    allShips[i] = basherShip;
+                    break;
+                case "a":
+                    Aggressive aggressiveShip = new Aggressive();
+                    allShips[i] = aggressiveShip;
+                    break;
+                case "d":
+                    Drunkard drunkShip = new Drunkard();
+                    allShips[i] = drunkShip;
+                    break;
+                case "s":
+                    Special specialShip = new Special();
+                    allShips[i] = specialShip;
+                    break;
+                default:
+                    allShips[i] = null;
+                    break;
             }
         }
         return allShips;
