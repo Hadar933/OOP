304b35e7f20ec0510b9e5cd2873de7ac7c6ad99c
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-09-03 13:05:49 +0300

    added facade skeleton

--- ex4/src/CollectionFacadeSet.java
+++ ex4/src/CollectionFacadeSet.java
@@ -1,2 +1,54 @@
-public class CollectionFacadeSet {
+/**
+ * Wraps an underlying Collection and serves to both simplify its API and give it a common type with the
+ * implemented SimpleHashSets.
+ */
+public class CollectionFacadeSet implements SimpleSet{
+
+	protected java.util.Collection<java.lang.String> collection;
+
+	/**
+	 * Creates a new facade wrapping the specified collection.
+	 * @param collection - The Collection to wrap.
+	 */
+	public CollectionFacadeSet(java.util.Collection<java.lang.String> collection){
+
+	}
+
+	/**
+	 * Add a specified element to the set if it's not already in it.
+	 * @param newValue New value to add to the set
+	 * @return - False iff newValue already exists in the set
+	 */
+	@Override
+	public boolean add(java.lang.String newValue) {
+		return false;
+	}
+
+	/**
+	 *Look for a specified value in the set.
+	 * @param searchVal Value to search for
+	 * @return True iff searchVal is found in the set
+	 */
+	@Override
+	public boolean contains(java.lang.String searchVal) {
+		return false;
+	}
+
+	/**
+	 * Remove the input element from the set.
+	 * @param toDelete Value to delete
+	 * @return True iff toDelete is found and deleted
+	 */
+	@Override
+	public boolean delete(java.lang.String toDelete) {
+		return false;
+	}
+
+	/**
+	 * @return The number of elements currently in the set
+	 */
+	@Override
+	public int size() {
+		return 0;
+	}
 }
