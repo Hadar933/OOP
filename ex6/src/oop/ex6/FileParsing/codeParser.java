b24242f9ac4fbe56c894c6ef314868af2e9f9efe
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-09-17 19:08:02 +0300

    minor fixes

--- ex6/src/oop/ex6/FileParsing/codeParser.java
+++ ex6/src/oop/ex6/FileParsing/codeParser.java
@@ -11,9 +11,10 @@ import java.util.Scanner;
  */
 public class codeParser {
 	private static final String EMPTY_LINE = "";
+	private static final String SPACE = " ";
 
 	/**
-	 * converts sJava to array of string, and remove all spaces
+	 * converts sJava to array of string, and remove all spaces and comments
 	 * @param fileName - some sJava file name
 	 * @return - ArrayList of lines
 	 * @throws FileNotFoundException - if the file doesn't exist
@@ -23,27 +24,11 @@ public class codeParser {
 		Scanner scanner = new Scanner(sJava);
 		ArrayList<String> data = new ArrayList<>();
 		while (scanner.hasNext()) {
-			String line = cleanLine(scanner.nextLine());
+			String line = scanner.nextLine().replaceAll("\\s+", SPACE).replaceAll("//.*", EMPTY_LINE);
 			if (!line.equals(EMPTY_LINE)) {
 				data.add(line);
 			}
 		}
 		return data;
 	}
-
-	/*
-	given some line, remove all excess spaces and comments
-	 */
-	private static String cleanLine(String line) {
-		return line.replaceAll("\\s+", " ").replaceAll("//.*", "");
-	}
-
-	public static void main(String[] args) throws FileNotFoundException {
-		String fileName = args[0];
-		ArrayList<String> data = sJava2StringArray(fileName);
-		for (String line : data) {
-			System.out.println(line);
-		}
-	}
-
 }
